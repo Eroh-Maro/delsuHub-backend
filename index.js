c5fa6be3848fbf7a9b21f2ac55204675935a6800
Eroh Oghenemaro Divine <maroeroh@gmail.com>
2025-06-20 15:36:58 +0100

    space

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import cors from 'cors';
 dotenv.config();
 const port = process.env.PORT || 5000; 
 
+
 app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:5173', "https://delsu-hub-frontend-i7fo.vercel.app"],
